517df43290be2d62cd5fdeb7e2413bc9cca35ecf
darhan1168 <darinahanina04@gmail.com>
2023-09-11 18:40:27 +0200

    Change DateOnly on DateTime

--- Planner/Planner/Core/Models/DailyRoutineTask.cs
+++ Planner/Planner/Core/Models/DailyRoutineTask.cs
@@ -5,5 +5,5 @@ public class DailyRoutineTask : BaseEntity
     public string Name { get; set; }
     public string Description { get; set; }
     public bool IsDone { get; set; }
-    public DateOnly Date { get; set; }
+    public DateTime Date { get; set; }
 }
--- Planner/Planner/Core/Models/Reminder.cs
+++ Planner/Planner/Core/Models/Reminder.cs
@@ -4,5 +4,5 @@ public class Reminder : BaseEntity
 {
     public string Name { get; set; }
     public string Description { get; set; }
-    public DateOnly Date { get; set; }
+    public DateTime Date { get; set; }
 }
