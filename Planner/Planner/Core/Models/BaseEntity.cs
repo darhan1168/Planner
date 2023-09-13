026ef2c5d948aeb6e90122c842418a6099a95f04
darhan1168 <darinahanina04@gmail.com>
2023-09-13 16:06:05 +0200

    Add DatabaseGeneratedOption for Id in baseEntity

--- Planner/Planner/Core/Models/BaseEntity.cs
+++ Planner/Planner/Core/Models/BaseEntity.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Planner.Core.Models;
 
 public abstract class BaseEntity
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 }
