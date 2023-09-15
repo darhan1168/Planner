a591f79c427816ade081e68e143eb21fd4311b94
darhan1168 <darinahanina04@gmail.com>
2023-09-15 14:28:48 +0200

    Add CreateTask View

--- Planner/Planner/Controllers/PlannerController.cs
+++ Planner/Planner/Controllers/PlannerController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Planner.BLL.Interfaces;
+using Planner.Core.Helpers;
+using Planner.Core.Models;
+using Planner.Models;
 
 namespace Planner.Controllers;
 
@@ -8,10 +11,12 @@ namespace Planner.Controllers;
 public class PlannerController : Controller
 {
     private readonly IDailyTaskService _dailyTaskService;
+    private readonly IUserService _userService;
 
-    public PlannerController(IDailyTaskService dailyTaskService)
+    public PlannerController(IDailyTaskService dailyTaskService, IUserService userService)
     {
         _dailyTaskService = dailyTaskService;
+        _userService = userService;
     }
     
     [HttpGet]
@@ -32,5 +37,52 @@ public class PlannerController : Controller
         }
         
         return View(getTasksResult.Data);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> CreateTask()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateTask(DailyRoutineTaskViewModel dailyRoutineTaskModel)
+    {
+        if (User.Identity == null)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        if (!ModelState.IsValid)
+        {
+            TempData["Error"] = $"Invalid {nameof(dailyRoutineTaskModel)}";
+            
+            return View(dailyRoutineTaskModel);
+        }
+        
+        var getUserResult = await _userService.GetUserByUsername(User.Identity.Name);
+        
+        if (!getUserResult.IsSuccessful)
+        {
+            TempData["Error"] = getUserResult.Message;
+            
+            return View(dailyRoutineTaskModel);
+        }
+
+        var dailyRoutineTask = new DailyRoutineTask();
+        dailyRoutineTaskModel.MapTo(dailyRoutineTask);
+        dailyRoutineTask.User = getUserResult.Data;
+        dailyRoutineTask.IsDone = false;
+        
+        var addResult = await _dailyTaskService.AddDailyTask(dailyRoutineTask);
+        
+        if (!addResult.IsSuccessful)
+        {
+            TempData["Error"] = addResult.Message;
+            
+            return View(dailyRoutineTaskModel);
+        }
+        
+        return RedirectToAction("Index");
     }
 }
